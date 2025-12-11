fbecfe5ab88458ede882208888f8fb3484469318
Kerolos  Edwar <wassofgoug@gmail.com>
2025-12-12 01:09:29 +0200

    Update The Code.py

--- The Code.py
+++ The Code.py
@@ -2,6 +2,49 @@ import os
 books={}
 def clear_screen():
   os.system("cls" if os.name=="nt" else "clear")
+
+def add_book():
+    clear_screen()
+    isbn=int(input("Enter ISBN :"))
+    titl=input("Enter The Titl :")
+    author=input("Enter The Author :")
+    books[isbn]={}
+    books[isbn]['name']=titl
+    books[isbn]['katp']=author
+    books[isbn]['available']=True
+    print(f" The Book '{books[isbn]['name']}' By '{books[isbn]['katp']}' Has Added Successfully!")
+
+def list_books():
+  clear_screen()
+  for var in (books):
+    print(f"ISBN: {var}, Titl: {books[var]['name']}, Author: {books[var]['katp']}  available:{books[var]['available']}")
+
+def check_out():
+  clear_screen()
+  theb=int((input("Enter ISBN to check out :")))
+  if theb in books:
+    if books[theb]['available']==True:
+      books[theb]['available']=False
+      print(f"{books[theb]['name']} Checked out successfully...")
+    elif books[theb]['available']==False:
+      print(f"Sorry {books[theb]['name']} Is NOT available")
+    else:
+      print(f"Sorry I can't Find {theb} ")
+
+def check_in():
+  clear_screen()
+  theb=int(input("Enter ISBN To check in :"))
+  if theb in books:
+    if books[theb]['available']==False:
+      print(f"{books[theb]['name']} Checked in Successfully !")
+      books[theb]['available']=True
+    elif books[theb]['available']==True:
+      print(f"{books[theb]['name']} Already available !")
+  else:
+    print(f"Sorry I Can't Find {theb}")
+  
+
+  
 def home():
   print("Menu:")
   print("1. Add Book")
@@ -15,50 +58,28 @@ while True:
   choice=int(input(":"))
   #Adding An Book
   if choice==1:
-    clear_screen()
     while True:
-      clear_screen()
-      isbn=int(input("Enter ISBN :"))
-      titl=input("Enter The Titl :")
-      author=input("Enter The Author :")
-      books[isbn]={}
-      books[isbn]['name']=titl
-      books[isbn]['katp']=author
-      books[isbn]['available']=True
-      print(f" The Book '{books[isbn]['name']}' By '{books[isbn]['katp']}' Has Added Successfully!")
-      dis=input("Do You Want To Add More Books? (y/n)")
-      if dis=='n':
+      add_book()
+      keep_going=input("Do you Want To Add Anthor Book?(y/n)").lower()
+      if keep_going=='n':
         break
-
   #list books
   elif choice==4:
-    clear_screen()
-    for var in (books):
-      print(f"ISBN: {var}, Titl: {books[var]['name']}, Author: {books[var]['katp']} ")
+    list_books()
   #Check out Book 
   elif choice ==2:
-    clear_screen()
-    theb=int((input("Enter ISBN to check out :")))
-    if theb in books:
-      if books[theb]['available']==True:
-        books[theb]['available']=False
-        print(f"{books[theb]['name']} Checked out successfully...")
-      elif books[theb]['available']==False:
-        print(f"Sorry {books[theb]['name']} Is NOT available")
-    else:
-      print(f"Sorry I can't Find {theb} ")
+    while True:
+      check_out()
+      keep_going=input("Do you Want To check out Anthor Book?(y/n)").lower()
+      if keep_going=='n':
+        break
   #Check In Book
   elif choice==3:
-    clear_screen()
-    theb=int(input("Enter ISBN To check in :"))
-    if theb in books:
-      if books[theb]['available']==False:
-        print(f"{books[theb]['name']} Checked in Successfully !")
-        books[theb]['available']=True
-      elif books[theb]['available']==True:
-        print(f"{books[theb]['name']} Already available !")
-    else:
-      print(f"Sorry I Can't Find {theb}")
+    while True:
+      check_in()
+      keep_going=input("Do you Want To check out Anthor Book?(y/n)").lower()
+      if keep_going=='n':
+        break
   elif choice==5:
     print("\n Exit The Program....")
     break
